2ebce7c5fee57e79bb3ea01cd0b111394830f068
Alberto <vazquez26fig@gmail.com>
2025-04-08 10:41:58 -0600

    Agregar archivos de proyecto.

---
+++ Components/Data/Productos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial2.Components.Data
+{
+    public class Productos
+    {
+        [Required(ErrorMessage = "El inventario es requerido.")]
+        [Range(1, int.MaxValue, ErrorMessage = "El inventario debe ser mayor a 0.")]
+        public int Inventario { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido.")]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "Debe seleccionar un departamento.")]
+        public string? Departamento { get; set; }
+    }
+
+}
